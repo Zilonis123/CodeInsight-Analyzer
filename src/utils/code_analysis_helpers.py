f54a3cc73c76c723b0cb0096627c6dc21b5403f6
Zilonis123 <kazaks.karlis2009@gmail.com>
2024-01-03 21:14:18 +0200

    Fix is_text_file

--- src/utils/code_analysis_helpers.py
+++ src/utils/code_analysis_helpers.py
@@ -2,7 +2,10 @@ import mimetypes
 
 def is_text_file(file_path: str) -> bool:
     # check if a file is a text file
-    mime: str = mimetypes.guess_type(file_path)[0]
 
-    isText = "text" in mime or ".json" in file_path # account for .json displaying as "application/json"
-    return isText
+    try:
+        mime: str = mimetypes.guess_type(file_path)[0]
+        isText = "text" in mime or ".json" in file_path # account for .json displaying as "application/json"
+        return isText
+    except:
+        return False
