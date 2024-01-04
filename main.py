4cd03f2694e2a5e793b9ab01bb287da00bf5e230
Zilonis123 <kazaks.karlis2009@gmail.com>
2024-01-04 21:24:46 +0200

    Simple hiearchy displau

--- main.py
+++ main.py
@@ -4,28 +4,44 @@ from ttkthemes import ThemedTk
 
 import os, json
 from src.analysis.code_analyzer import *
+from src.analysis.folder_hierarchy import build_folder_hierarchy
 
-def search() -> None:
+
+def count_file_types(target_directory: str):
     def draw_output(file_count: dict) -> None:
         output = ""
         for file_ext, count in file_count.items():
             output += f"{file_ext} found {count} times\n"
-        output_string.set(output)
+        file_output_string.set(output)
+   
+    file_count: dict = process_directory(target_directory, excluded, draw_output)
+
+
+def display_folder_hiearchy(target_directory: str):
+    hiearchy = build_folder_hierarchy(target_directory, excluded.get("folders", []))
     
+    folder_output_string.set(json.dumps(hiearchy, indent=2))
+
+def search() -> None:
     target_directory: str = entry_string.get()
+    
+    if target_directory == "":
+        # nothing provided
+        return
     
     # reset input and output
     entry_string.set("")
-    output_string.set("")
+    file_output_string.set("")
+    folder_output_string.set("")
+    
     
     if not os.path.exists(target_directory):
         # path provided doesn't exist
-        output_string.set(f"Error: Provided path ({target_directory}) is incorrect!")
+        file_output_string.set(f"Error: Provided path ({target_directory}) is incorrect!")
         return
-    
-    file_count: dict = process_directory(target_directory, excluded, draw_output)
-    
-    
+
+    count_file_types(target_directory)
+    display_folder_hiearchy(target_directory)
 
 if __name__ == "__main__":
     # get excluded 
@@ -36,7 +52,7 @@ if __name__ == "__main__":
 
     window = ThemedTk(theme="black")
     window.title("Code Analyzer")
-    window.geometry("600x400")
+    window.geometry("750x550")
     window.resizable(False, False)
     
 
@@ -51,11 +67,23 @@ if __name__ == "__main__":
     button.pack(side="left")
 
     # output
-    output_string = tk.StringVar()
-    output_label = ttk.Label(master=window, textvariable=output_string)
+    output_frame = ttk.Frame(master=window)
+    file_output_string = tk.StringVar()
+    file_output_label = ttk.Label(master=output_frame, textvariable=file_output_string)
     
-    # layout
-    input_frame.pack(side="left", expand=True, fill="both")
-    output_label.pack(side="right", expand=True, fill="both")
+    folder_output_string = tk.StringVar()
+    folder_output_label = ttk.Label(master=output_frame, textvariable=folder_output_string)
+    
+    file_output_label.pack(side="left")
+    folder_output_label.pack(side="right")
+    
+    
+    # layout (grid)
+    window.columnconfigure(0, weight=1)
+    window.columnconfigure(1, weight=2)
+    window.rowconfigure(0, weight=1)
+
+    input_frame.grid(row=0, column=0, sticky="nesw")
+    output_frame.grid(row=0, column=1, sticky="nesw")
 
     window.mainloop()
--- src/analysis/folder_hierarchy.py
+++ src/analysis/folder_hierarchy.py
@@ -1,4 +1,4 @@
-import os, json
+import os
 
 
 def build_folder_hierarchy(root_path, excluded) -> dict:
@@ -17,22 +17,3 @@ def build_folder_hierarchy(root_path, excluded) -> dict:
         return current_folder
 
     return traverse_folder(root_path)
-
-def print_folder_hierarchy(path: str, excluded: list) -> None:
-    hiearchy = build_folder_hierarchy(path, excluded)
-    
-    # helper function
-    # def print_h(folder_hierarchy, indentation=0):
-    #     if isinstance(folder_hierarchy, dict):
-    #         for folder, content in folder_hierarchy.items():
-    #             print("  " * indentation + f"{folder}/")
-    #             print_folder_hierarchy(content, indentation + 1)
-    #             files = content.get("files", [])
-    #             if files:
-    #                 print("  " * (indentation + 1) + "Files:")
-    #                 for file in files:
-    #                     print("  " * (indentation + 2) + file)
-
-    # print_h(hiearchy)
-
-    print(json.dump(hiearchy, indent=2))
