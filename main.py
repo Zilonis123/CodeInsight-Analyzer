031a842044c8bf894853b34a62709c0aacf23b63
Zilonis123 <kazaks.karlis2009@gmail.com>
2024-01-04 17:04:09 +0200

    Basic tkinter implementation

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
-import os, sys, json
-from tqdm import tqdm
-from colorama import Fore, Style
+import tkinter as tk
+from tkinter import ttk
 
+import os, json
 from src.analysis.code_analyzer import *
-from src.utils.file_operations import *
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python main.py <directory_path>")
-    else:
-        target_directory: str = sys.argv[1]
 
-        # get excluded folders
-        file = open("exclude.json")
-        excluded: dict = json.load(file)
-        file.close()
+def search() -> None:
+    target_directory: str = entry_string.get()
+    
+    # reset input and output
+    entry_string.set("")
+    output_string.set("")
+    
+    if not os.path.exists(target_directory):
+        # path provided doesn't exist
+        output_string.set(f"Error: Provided path ({target_directory}) is incorrect!")
+        return
+    
+    file_count: dict = process_directory(target_directory)
+    
+    output = ""
+    for file_ext, count in file_count.items():
+        output += f"{file_ext} found {count} times\n"
+        
+    output_string.set(output)
 
-        files: int = count_files(target_directory, excluded.get("folders", []))
 
-        print(f"Searching {Fore.GREEN}{files}{Style.RESET_ALL} files")
-        file_count: dict = process_directory(target_directory)
+# get excluded 
+file = open("exclude.json")
+excluded: dict = json.load(file)
+file.close()
 
-        for file_ext, count in file_count.items():
-            print(f"{file_ext} found {Fore.GREEN}{count}{Style.RESET_ALL} times")
+
+window = tk.Tk()
+window.title("Code Analyzer")
+window.geometry("300x150")
+
+
+# input
+input_frame = ttk.Frame(master=window)
+entry_string = tk.StringVar()
+entry = ttk.Entry(master=input_frame, textvariable=entry_string)
+button = ttk.Button(master=input_frame, text="Search", command=search)
+
+entry.pack(side="left", padx=10)
+button.pack(side="left")
+input_frame.pack()
+
+# output
+output_string = tk.StringVar()
+output_label = ttk.Label(master=window, textvariable=output_string)
+output_label.pack()
+
+window.mainloop()
